95f832acae75d2365aa3a79b902895a2d4863585
manisha-55 <mnpogul@gmail.com>
2025-10-12 16:54:09 +0530

    Day 1 — Python Basics: Printing, Variables and Input/Output

--- python-input.py
+++ python-input.py
@@ -1,4 +1,24 @@
-name = input("Enter your name: ")
-age= input("Enter your age: ")
-city= input("Enter your city: ")
-print("Hello "+name +", you are "+age+ " years old and live in "+ city+ "!" )
+# name = input("Enter your name: ")
+# age= input("Enter your age: ")
+# city= input("Enter your city: ")
+# print("Hello "+name +", you are "+age+ " years old and live in "+ city+ "!" )
+
+# Q1: Add two numbers
+# Write a program that takes two numbers from the user and prints their sum.
+# a= int(input("enter num1: "))
+# b= int(input("enter num2: "))
+# c=a+b
+# print("sum of given numbers is: ",c)
+
+# Q2: Celsius to Fahrenheit
+# Write a program that takes temperature in Celsius and converts it to Fahrenheit using: F = (C × 9/5) + 32
+# temp=int(input("enter the temp in celcius: "))
+# f=  (temp * 9/5) + 32
+# print("temp in fahrenheit is: ",f)
+
+# Q3: Calculate Age
+# Write a program that takes birth year and calculates current age.
+birth_yr=int(input("enter your birth year: "))
+current_yr =2025
+curr_age= current_yr-birth_yr
+print("you are",curr_age, "years old")
