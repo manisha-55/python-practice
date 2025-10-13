2a97017ec9538c87fb71387b054f70f42d9328e3
manisha-55 <mnpogul@gmail.com>
2025-10-13 22:24:13 +0530

    #Day 5 — Patterns & Loops

--- day5-patterns_loops.py
+++ day5-patterns_loops.py
@@ -5,9 +5,27 @@ for i in range(1,n+1):
     for j in range(0,i):
         print('*',end=' ')
     print()
+print()
 
 #Pattern 2 — Inverted Triangle
 for i in range(0,n):
     for j in range(0,n-i):
         print('*',end=' ')
+    print()
+print()
+
+# Pattern 3 — Right-Aligned Triangle
+for i in range(0,n):
+    for j in range(0,n-i-1):
+        print(' ',end='')
+    for j in range(0,i+1):
+        print('* ',end='')
+    print()
+print()
+
+for i in range(1,n+1):
+    for j in range(n-i):
+        print("  ",end='')
+    for j in range(i):
+        print("* ",end='')
     print()
